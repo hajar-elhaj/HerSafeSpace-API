ba6e0d049f73fe0b8a342c4ccb82c7e5e37670ba
Hajar <hajar.elhaj20@gmail.com>
2026-02-15 19:50:55 +0000

    Making the user fully anonymous in posts and comments

--- api/serializers.py
+++ api/serializers.py
@@ -24,7 +24,7 @@ class AnonymousPostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AnonymousPost
-        fields = ['id', 'user', 'category', 'content', 'created_at']
+        fields = ['id', 'category', 'content', 'created_at']  #user is fully anonymous (not showing in the output)
         read_only_fields = ['id', 'created_at']
 
 
@@ -33,5 +33,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'user', 'content', 'created_at']
+        fields = ['id', 'post', 'content', 'created_at'] #user is fully anonymous (not showing in the output)
         read_only_fields = ['id', 'created_at']
