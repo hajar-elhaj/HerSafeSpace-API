539d01b19c864aa03bd173e0561b5fdb6c25c415
Hajar <hajar.elhaj20@gmail.com>
2026-02-15 18:38:08 +0000

    Registering Models in Admin and applying migrations

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MoodEntry, SelfCare, AnonymousPost, Comment
 
-# Register your models here.
+admin.site.register(MoodEntry)
+admin.site.register(SelfCare)
+admin.site.register(AnonymousPost)
+admin.site.register(Comment)
